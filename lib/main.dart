a0eb6a8d6b5d083cb9e2d59f8489c471caf28302
zakiburnama <zaki.burnama1017@gmail.com>
2021-09-01 10:37:01 +0700

    finally working

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        //primarySwatch: Colors.blue,
       ),
       home: HomeScreen(),
     );
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
-import 'package:car_shop/constans.dart';
+//import 'package:car_shop/constans.dart';
+//import 'package:car_shop/screens/home/components/body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
 //import 'package:car_shop/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -8,37 +9,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      //appBar: buildAppBar(),
+      //body: Body(),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
-      elevation: 0,
+      elevation: 3,
       leading: IconButton(
-        icon: SvgPicture.asset("assets\icons\back.svg"), 
+        icon: Image.asset("assets/icons/left-arrow.png"),
         onPressed: () {},
       ),
-      actions: <Widget>[
-      IconButton(
-        icon: SvgPicture.asset(
-          "assets/icons/search.svg",
-          // By default our  icon color is white
-          color: kTextColor,
-        ),
-        onPressed: () {},
-      ),
-      IconButton(
-        icon: SvgPicture.asset(
-          "assets/icons/cart.svg",
-          // By default our  icon color is white
-          color: kTextColor,
-        ),
-        onPressed: () {},
-      ),
-      SizedBox(width: kDefaultPaddin / 2)
-    ],
+      
+      //      
     );
   }
 
