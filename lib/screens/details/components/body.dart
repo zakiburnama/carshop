a379b8ab331231d0f89bf046c600d04660bafc23
zakiburnama <zaki.burnama1017@gmail.com>
2021-09-04 08:05:16 +0700

    more details at itemCard

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -2,6 +2,8 @@ import 'package:car_shop/constans.dart';
 import 'package:car_shop/models/Product.dart';
 import 'package:flutter/material.dart';
 
+import 'product_title_with_image.dart';
+
 class Body extends StatelessWidget {
   final Product product;
 
@@ -34,63 +36,6 @@ class Body extends StatelessWidget {
         ],
       ),
       
-    );
-  }
-}
-
-class ProductTitleWithImage extends StatelessWidget {
-  const ProductTitleWithImage({
-    Key key,
-    @required this.product,
-  }) : super(key: key);
-
-  final Product product;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Ini adalah mobil",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            product.title,
-            style: Theme.of(context).textTheme.headline4.copyWith(
-              color: Colors.white, fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: kDefaultPaddin,),
-          Row(
-            children: [
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(text: "Price\n"),
-                    TextSpan(
-                      text: "\$${product.price}",
-                      style: Theme.of(context).textTheme.headline4.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(width: kDefaultPaddin,),
-              Expanded(
-                child: Image.asset(
-                  product.image,
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/details/components/product_title_with_image.dart
@@ -0,0 +1,61 @@
+import 'package:car_shop/models/Product.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constans.dart';
+
+class ProductTitleWithImage extends StatelessWidget {
+  const ProductTitleWithImage({
+    Key key,
+    @required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Ini adalah mobil",
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            product.title,
+            style: Theme.of(context).textTheme.headline4.copyWith(
+              color: Colors.white, fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: kDefaultPaddin,),
+          Row(
+            children: [
+              RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(text: "Price\n"),
+                    TextSpan(
+                      text: "\$${product.price}",
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: kDefaultPaddin,),
+              Expanded(
+                child: Image.asset(
+                  product.image,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
